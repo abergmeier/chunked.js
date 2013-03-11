c38ef11d53768de85e95375202f557ce6d21e55f
abergmeier <andreas.bergmeier@gmx.net>
2013-03-11 08:40:48 +0100

    XMLHttpRequest wrapper getting to completion.

--- streams.js
+++ streams.js
@@ -3,6 +3,10 @@
 	/**
 	 * @const
 	 */
+	var global = window;
+	/**
+	 * @const
+	 */
 	var STREAM = { 
 		/**
 		 * @const
@@ -30,7 +34,7 @@
 					}
 				};
 
-				req.header("Range", "bytes=0-999"
+				//req.header("Range", "bytes=0-999"
 			}
 		},
 		/**
@@ -63,10 +67,6 @@
 			},
 			load: function() {
 			},
-			run: function( req, url) {
-				req.open( "GET", url, true );
-				req.responseType = STREAM.MOZ.responseType;
-			}
 		}
 	};
 	
@@ -89,10 +89,13 @@
 				try {
 					// Try stream API
 					req.responseType = canidate.responseType;
-					console.log( "GOOD" + canidate );
+					// We have to check value, since the former line
+					// doesn't have to throw an exception according
+					// to spec.
+					if( req.responseType !== canidate.responseType )
+						continue;
 					return canidate;
 				} catch( ex ) {
-					console.log( "BAD" + ex );
 					// Ignore and go to next
 				}
 			}
@@ -139,153 +142,190 @@
 	(function() {
 		// Extend XMLHttpRequest
 		/**
-		 * @const
-		 */
-		var origRequest = window.XMLHttpRequest;
-		/**
 		 * @constructor
 		 * @extends XMLHttpRequest
 		 */
-		window.XMLHttpRequest = (function() {
-			/**
-			 * @type {boolean}
-			 */
-			var _is_stream = false;
+		window.XMLHttpRequest = (function( origRequest ) {
+			
 			/**
 			 * @const
+			 * @type {XMLHttpRequest}
 			 */
-			var _send = function( target, receiver, data ) {
-				if( data.prototype !== Stream.prototype )
-					return target.send( data );
+			var wrapper = function() {
+				/**
+				 * @type {boolean}
+				 */
+				var _is_stream = false;
+				/**
+				 * @const
+				 */
+				var _internal = new origRequest();
+				/**
+				 * @const
+				 * @param {String}
+				 * @param {String}
+				 * @param {boolean=} opt_param
+				 * @param {String=} opt_param
+				 * @param {String=} opt_param
+				 */
+				var _open = function( method, url, async, user, password ) {
+					if( async === undefined )
+						async = true;
+					
+					return _internal.open( method, url, async, user, password );
+				};
+				/**
+				 * @const
+				 */
+				var _send = function( data ) {
+					if( data.isPrototypeOf( global.Stream ) ) {
+						// Is Stream
+						return _internal.send( data._internal );
+					}
+					
+					return _internal.send( data );
+				};
+				/**
+				 * @const
+				 */
+				var fix = function( func ) {
+					return func.bind( _internal );
+				};
+				Object.defineProperties(
+					this,
+					{
+						"responseType": { get: function() {
+						                       	if( _is_stream )
+						                       		return STREAM.API.responseType;
+						                       	return _internal.responseType;
+						                  },
+						                  set: function( val ) {
+						                       	if( val === "stream" ) {
+						                       		// Add special stream handling
+						                       		_is_stream = true;
+						                       		val = streamMethod.responseType;
+						                       	} else
+						                       		_is_stream = false;
+						                       _internal.responseType = val;
+						                  } },
+						"response"    : { get: function() {
+						                       	if( _is_stream ) {
+						                       		if( _internal.error )
+						                       			return null;
+						                       		else if( _internal.readyState === this.LOADING ) {
+						                       			return new Stream( _internal.response );
+						                       		}
+						                       	}
+						                       	return _internal.response;
+						                  } },
+						"readyState"           : { get: function() { return _internal.readyState;   } },
+						"open"                 : { value: _internal.open.bind(_internal             ) },
+						"setRequestHeader"     : { value: _internal.open.bind(_internal             ) },
+						"upload"               : { get: function() { return _internal.upload;       } },
+						"send"                 : { value: _internal.send.bind(_internal             ) },
+						"abort"                : { value: _internal.abort.bind(_internal            ) },
+						"status"               : { get: function() { return _internal.status;       } },
+						"statusText"           : { get: function() { return _internal.statusText;   } },
+						"getResponseHeader"    : { value: fix(_internal.getResponseHeader           ) },
+						"getAllResponseHeaders": { value: fix(_internal.getAllResponseHeaders       ) },
+						"overrideMimeType"     : { value: fix(_internal.overrideMimeType            ) },
+						"responseText"         : { get: function() { return _internal.responseText; } },
+						"responseXML"          : { get: function() { return _internal.responseXML;  } },
+						"addEventListener"     : { value: fix(_internal.addEventListener            ) }
+					}
+				);
 				
-				// Is Stream
-				return target.send( data._internal );
-			};
-			/**
-			 * Handler to extends XMLHttpRequest according to
-			 * Streams API
-			 * @struct
-			 */
-			var proxyHandler = {
-				/**
-				 * @return {*}
-				 */
-				get: function( target, name, receiver) {
-					if( _is_stream ) {
-						if( name === "responseType")
-							return STREAM.API.responseType;
+			}
 			
-						if( name === "response" ) {
-							if( receiver.error )
-								return null;
-							else if( readyState === receiver.LOADING ) {
-								return new Stream( receiver[name] );
-							}
-						}
-					}
-					
-					if( name === "send" )
-						return function( data ) {
-							return _send( target, receiver, data );
-						};
-					
-					return target[name];
-				},
-				/**
-				 * @return {boolean}
-				 */
-				set: function( target, name, val, receiver) {
-					if( name === "responseType" ) {
-						if( val === "stream" ) {
-							// Add special stream handling
-							_is_stream = true;
-							val = streamMethod.responseType;
-						} else
-							_is_stream = false;
-					}
-					
-					return target[name] = val;
-				} 
-			};
-			return new Proxy( origRequest, proxyHandler );
-		})();
+			wrapper.prototype = new origRequest();
+/*			
+			Object.defineProperty( proxy,
+			                       "LOADING",
+			                       { value: origRequest.LOADING,
+			                         writeable: false }
+			);
+*/
+			return wrapper;
+		})( window.XMLHttpRequest );
 	})();
 	
 	/**
 	 * @constructor
 	 */
-	window.StreamReader |= function() {
-		/**
-		 * @struct
-		 */
-		var reader = {
-			/** 
-			 * @param {Stream}
-			 * @param {number}
-			 */
-			readAsBlob: function( stream, maxSize ) {
-			},
+	window.StreamReader = window.StreamReader || (function() {
+		var constructor = function() {
 			/**
-			 * @param {Stream}
-			 * @param {number=} opt_argument
-			 */
-			readAsArrayBuffer: function( stream, maxSize ) {
-				maxSize = maxSize || stream._internal.byteLength;
-				result = stream._internal.slice( 0, maxSize );
-			},
-			/**
-			 * @param {Stream}
-			 * @param {String=} opt_argument
-			 * @param {number=} opt_argument
-			 */
-			readAsText: function( stream, encoding, maxSize ) {
-			},
-			/**
-			 * @param {Stream}
-			 * @param {number=} opt_argument
-			 */
-			readAsDataURL: function( Stream, maxSize ) {
-			},
-			abort: function() {
-			},
-			/**
-			 * @const
 			 * @type {number}
 			 */
-			EMPTY: 0,
+			var _readyState = constructor.EMPTY;
 			/**
-			 * @const
-			 * @type {number}
-			 */
-			LOADING: 1,
-			/**
-			 * @const
-			 * @type {number}
-			 */
-			DONE: 2,
-			/**
-			 * readonly attribute
-			 * @type {number}
-			 */
-			readyState: 0,
-			/**
-			 * readonly attribute
 			 * @type {*}
 			 */
-			result: null,
+			var _result     = null;
 			/**
-			 * readonly attribute
 			 * @type {StreamError}
 			 */
-			error      : null,
-			onloadstart: null,
-			onprogress : null,
-			onload     : null,
-			onabort    : null,
-			onerror    : null,
-			onloadend  : null
+			var _error      = null;
+			return Object.create(
+				{
+					/** 
+					 * @param {Stream}
+					 * @param {number}
+					 */
+					readAsBlob: function( stream, maxSize ) {
+					},
+					/**
+					 * @param {Stream}
+					 * @param {number=} opt_argument
+					 */
+					readAsArrayBuffer: function( stream, maxSize ) {
+						maxSize = maxSize || stream._internal.byteLength;
+						result = stream._internal.slice( 0, maxSize );
+					},
+					/**
+					 * @param {Stream}
+					 * @param {String=} opt_argument
+					 * @param {number=} opt_argument
+					 */
+					readAsText: function( stream, encoding, maxSize ) {
+					},
+					/**
+					 * @param {Stream}
+					 * @param {number=} opt_argument
+					 */
+					readAsDataURL: function( Stream, maxSize ) {
+					},
+					abort: function() {
+					},
+					onloadstart: null,
+					onprogress : null,
+					onload     : null,
+					onabort    : null,
+					onerror    : null,
+					onloadend  : null
+				},
+				// Add properties
+				{
+					"readyState": { get: function() {
+					                     	return _readyState;
+					                } },
+					"result"    : { get: function() {
+					                     	return _result;
+					                } },
+					"error"     : { get: function() {
+					                     	return _error;
+					                } }
+				}
+			);
 		};
-		return reader;
+		
+		Object.defineProperties( constructor,
+		                         { "EMPTY"  : { value: 0, writeable: false },
+		                           "LOADING": { value: 1, writeable: false },
+		                           "DONE"   : { value: 2, writeable: false }
+		                         }
+		);
+		return constructor;
 	})();
-	
+
 })();
