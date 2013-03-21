e9c1ccee40358961b2323fea61af0f41a8c0bae8
abergmeier <andreas.bergmeier@gmx.net>
2013-03-21 19:02:08 +0100

    Implemented basic HTTP range loading.

--- chunked.js
+++ chunked.js
@@ -11,9 +11,10 @@
 	 */
 	var global = config.is_worker ? self : window;
 	
-	var CHUNKED_RESPONSE_TYPES = { text       : "chunked-text"       ,
-	                               arraybuffer: "chunked-arraybuffer",
-	                               blob       : "chunked-blob"         };
+	var CHUNKED_RESPONSE_TYPES = { TEXT       : "chunked-text"       ,
+	                               ARRAYBUFFER: "chunked-arraybuffer",
+	                               BLOB       : "chunked-blob"         };
+	var mozArrayBuffer = "moz-chunked-arraybuffer";
 	/**
 	 * @const
 	 */
@@ -32,6 +33,9 @@
 			responseType: "blob",
 			partial_arraybuffer: function( request, callback ) {
 			},
+			RESPONSE_TYPE_MAP: { "chunked-text"       : "text"       ,
+			                     "chunked-arraybuffer": "arraybuffer",
+			                     "chunked-blob"       : "blob"         },
 			/**
 			 * Load every chunk separately, so we can provide partial download
 			 * data
@@ -41,20 +45,23 @@
 			 * @param
 			 * @param {number=} opt_param
 			 */
-			load_chunked: function( req, url, callbacks, length) {
-				var range = { chunk: 1000,
-				              start : 0,
-				              end   : 1000,
-				              length: length
-				};
+			load_chunked: function( req, callbacks, length) {
+				var range = (function( length ) {
+					var chunk = Math.min( length / 100 , 1024 * 1024 );
+					return { chunk : chunk,
+					         start : 0,
+					         end   : chunk,
+					         length: length
+					};
+				})( length );
 				/**
 				 * chain function gets called repeatedly, till all chunks are
 				 * read
 				 * @const
 				 */
 				var chain = function() {
-					req.open( "GET", url, config.async );
-					req.responseType = VARIANTS.NONE.responseType;
+					req.open( "GET", this.internal.url, this.internal.async, this.internal.user, this.internal.password );
+					req.responseType = VARIANTS.NONE.RESPONSE_TYPE_MAP[ this.internal.responseType ];
 					(function() {
 						/**
 						 * Special request handler
@@ -62,11 +69,21 @@
 						 */
 						var value = "bytes=" + range.start + "-" + range.end;
 						req.setRequestHeader( "Range", value );
-					})();
+					}).call( this );
 					req.onload = function( event ) {
+						(function() {
+							var progressEvent = Object.create( event, {
+								"lengthComputable": { value: true         },
+								"loaded"          : { value: range.end    },
+								"total"           : { value: range.length }
+							} );
+							this.internal.response = req.response;
+							callbacks.progress.call( this, progressEvent );
+						}).call( this );
+						
 						if( range.end === range.length ) {
 							// Downloaded everything successfully
-							callbacks.load( event );
+							callbacks.load.call( this, event );
 							return;
 						}
 						
@@ -75,12 +92,14 @@
 						range.end   += range.chunk;
 						range.end   = Math.min( range.end, range.length );
 						global.setTimeout( chain, 1 );
-					};
+					}.bind( this );
 					req.onerror = function( event ) {
-						// Fallback to whole file download
-						VARIANTS.NONE.load_complete( req, url );
-					};
-				};
+						this.internal.response = null;
+						// Fall back to whole file download
+						VARIANTS.NONE.load_complete.call( this, req, url );
+					}.bind( this );
+					req.send();
+				}.bind( this );
 				chain();
 			},
 			/**
@@ -90,39 +109,79 @@
 			 * @param {String}
 			 * @param
 			 */
-			load_complete: function( req, config, callbacks ) {
-				req.open( "GET", config.url, config.async );
+			load_complete: function( req, callbacks ) {
+				req.open( "GET", this.internal.url, this.internal.async, this.internal.user, this.internal.password );
 				req.responseType = VARIANTS.NONE.responseType;
 				req.onload = function( event ) {
-					callbacks.load();
-				};
+					callbacks.load.call( this );
+				}.bind( this );
 				req.onerror = function( event ) {
-					callbacks.error();
-				};
+					callbacks.error.call( this )();
+				}.bind( this );
 				req.send();
 			},
 			IGNORED_METHODS: [ "CONNECT", "HEAD" ],
 			handles: function( config ) {
 				return !( config.method in VARIANTS.NONE.IGNORED_METHODS );
 			},
-			send: function( req, config, callbacks ) {
-				if( config.method )
-				req.open( "HEAD", config.url, config.async );
-				req.responseType = "";
+			send: function( req, callbacks ) {
+				req.open( "HEAD", this.internal.url, this.internal.async, this.internal.user, this.internal.password );
+				req.responseType = "text";
 				req.onload = function( event ) {
 					var length = req.getResponseHeader( "Content-Length" );
 					length = Number( length );
-					if( length == 0 )
-						VARIANTS.NONE.load_complete( req, config, callbacks );
+					if( length === 0 )
+						VARIANTS.NONE.load_complete.call( this, req, callbacks );
 					else
-						VARIANTS.NONE.load_chunked( req, config, callbacks, length );
-				};
+						VARIANTS.NONE.load_chunked.call( this, req, callbacks, length );
+				}.bind( this );
 				req.onerror = function( event ) {
 					// TODO: Only fall back to load, when error
 					// is recoverable!
-					VARIANTS.NONE.load_complete( req, config, callbacks );
-				};
+					VARIANTS.NONE.load_complete.call( this, req, config, callbacks );
+				}.bind( this );
 				req.send();
+			},
+			/**
+			 * @param {String}
+			 * @param
+			 * @param {boolean=} opt_param
+			 */
+			"addEventListener": function( type, listener, useCapture ) {
+				if( !(type in VARIANTS.NONE.LISTENERS) )
+					throw new RangeError("'" + type + " not handled.");
+				
+				// Handle progress
+				//TODO: Handle useCapture
+				this.listeners = this.listeners || Object.create( VARIANTS.NONE.LISTENERS );
+				
+				var registry = this.listeners[type];
+				registry.push( listener );
+			},
+			"removeEventListener": function( type, listener, useCapture ) {
+				if( !(type in VARIANTS.NONE.LISTENERS) )
+					throw new RangeError("'" + type + " not handled.");
+				
+				this.listeners = this.listeners || Object.create( VARIANTS.NONE.LISTENERS );
+				
+				var registry = this.listeners[type];
+				for( var i = registry.length - 1; i >= 0; --i ) {
+					if( registry[i] === listener ) {
+						registry.splice( i, 1 );
+						break;
+					}
+				}
+			},
+			/**
+			 * @const
+			 */
+			LISTENERS: { "loadstart": [],
+			             "progress" : [],
+			             "abort"    : [],
+			             "error"    : [],
+			             "load"     : [],
+			             "timeout"  : [],
+			             "loadend"  : [] 
 			}
 		},
 		/**
@@ -143,13 +202,12 @@
 			handles: function( config ) {
 				return false;
 			},
-			FLAGS: { TEXT       : 1 << 1,
-			         ARRAYBUFFER: 1 << 2,
-			         BLOB       : 1 << 3
-			},
-			FLAG_MAPPINGS: { "chunked-text"       : VARIANTS.STREAM_API.FLAGS.TEXT       ,
-			                 "chunked-arraybuffer": VARIANTS.STREAM_API.FLAGS.ARRAYBUFFER,
-			                 "chunked-blob"       : VARIANTS.STREAM_API.FLAGS.BLOB
+			/**
+			 * @dict
+			 */
+			FLAGS: { "chunked-text"       : 1 << 1,
+			         "chunked-arraybuffer": 1 << 2,
+			         "chunked-blob"       : 1 << 3
 			},
 			/**
 			 * @const
@@ -159,11 +217,11 @@
 			 */
 			extract_response: function( flags, reader, req ) {
 				var response;
-				if( flags | VARIANTS.STREAM_API.FLAGS.TEXT )
+				if( flags | VARIANTS.STREAM_API.FLAGS[CHUNKED_RESPONSE_TYPES.TEXT] )
 					response = reader.readAsBlob( req.response );
-				else if( flags | VARIANTS.STREAM_API.FLAGS.ARRAYBUFFER )
+				else if( flags | VARIANTS.STREAM_API.FLAGS[CHUNKED_RESPONSE_TYPES.ARRAYBUFFER] )
 					response = reader.readAsArrayBuffer( req.response );
-				else if( flags | VARIANTS.STREAM_API.FLAGS.TEXT)
+				else if( flags | VARIANTS.STREAM_API.FLAGS[CHUNKED_RESPONSE_TYPES.BLOB] )
 					response = reader.readAsText( req.response );
 				else
 					console.log("Unhandled flags" + flags);
@@ -217,7 +275,7 @@
 			 * @struct
 			 */
 			CHUNKED_MAPPINGS: { "chunked-text"       : "moz-chunked-text",
-			                    "chunked-arraybuffer": "moz-chunked-arraybuffer",
+			                    "chunked-arraybuffer": mozArrayBuffer,
 			                    "chunked-blob"       : "moz-chunked-blob"
 			},
 			/**
@@ -228,7 +286,10 @@
 			handles: function( config ) {
 				return config.responseType in VARIANTS.MOZ.CHUNKED_MAPPINGS;
 			},
-			responseType: VARIANTS.MOZ.CHUNKED_MAPPINGS[ "chunked-arraybuffer" ],
+			/**
+			 * @const
+			 */
+			RESPONSE_TYPE: mozArrayBuffer,
 			send: function( req, config, callbacks ) {
 				req.open( config.method,
 				          config.url,
@@ -263,11 +324,11 @@
 				canidate = canidates[i];
 				try {
 					// Try stream API
-					req.responseType = canidate.responseType;
+					req.responseType = canidate.RESPONSE_TYPE;
 					// We have to check value, since the former line
 					// doesn't have to throw an exception according
 					// to spec.
-					if( req.responseType !== canidate.responseType )
+					if( req.responseType !== canidate.RESPONSE_TYPE )
 						continue;
 					return canidate;
 				} catch( ex ) {
@@ -276,7 +337,7 @@
 			}
 			return VARIANTS.NONE;
 		}
-	)( [VARIANTS.STREAM_API, VARIANTS.MOZ] );
+	)( [VARIANTS.STREAM_API/*, VARIANTS.MOZ*/] );
 	
 	/**
 	 * @const
@@ -321,16 +382,22 @@
 				/**
 				 * @const
 				 */
+				var instance = this;
+				/**
+				 * @const
+				 */
 				var internal = {
 					/**
 					 * @type {boolean}
 					 */
 					is_chunked: false,
-					
+					responseType: "",
 					method: null,
+					url: null,
 					async: true,
 					user: null,
 					password: null,
+					data:     undefined,
 					/**
 					 * @const
 					 * @param {String}
@@ -344,6 +411,7 @@
 							async = true;
 						
 						internal.method   = method;
+						internal.url      = url;
 						internal.async    = async;
 						internal.user     = user;
 						internal.password = password;
@@ -359,34 +427,39 @@
 					 * @const
 					 */
 					send: function( data ) {
-						/**
-						 * @const
-						 * @struct
-						 */
-						var config = { method      : internal.method      ,
-						               asyc        : internal.async       ,
-						               user        : internal.user        ,
-						               password    : internal.password    ,
-						               responseType: internal.responseType,
-						               data        : data
-						};
-						if( config.responseType in CHUNKED_RESPONSE_TYPES
-						 || streamMethod.handles( config ) ) {
+						internal.responseType = this.responseType;
+						internal.data = data;
+						internal.is_chunked = internal.responseType in CHUNKED_RESPONSE_TYPES
+						                   || streamMethod.handles( internal );
 
-							streamMethod.send( this, config, callbacks );
+						if( internal.is_chunked ) {
+							var callbacks = {
+								load: function( event ) {
+									
+								},
+								progress: function( event ) {
+									if( !internal.listeners )
+										return;
+									for( var i = 0; i != internal.listeners.length; ++i ) {
+										internal.listeners[i].progress( event );
+									}
+								}
+							};
+							streamMethod.send.bind( instance )( base, callbacks );
 						} else {
+							base.responseType = responseType;
+							base.open( internal.method, internal.url,
+							           internal.async, internal.user,
+							           internal.password );
 							base.send( data );
 						}
+						internal.data = undefined;
 					},
 					getResponse: function() {
-						if( internal.is_stream ) {
-							if( base.error )
-								return null;
-							else if( base.readyState === this.LOADING ) {
-								return new Stream( base.response );
-							}
-						}
-						return base.response;
+						if( internal.is_chunked )
+							return internal.response;
+						else
+							return base.response;
 					}
 				};
 
@@ -396,29 +469,40 @@
 				var fix = function( funcName ) {
 					return base[funcName].bind( base );
 				};
+				/**
+				 * @const
+				 */
+				var overwrite = function( funcName ) {
+					var method = streamMethod[funcName];
+					if( method )
+						return method.bind( instance );
+					
+					return fix( funcName );
+				};
 				Object.defineProperties(
 					this,
-					{
-						"responseType"         : { value: fix("responseType"                     ) },
-						"response"             : { get  : internal.getResponse                     },
-						"readyState"           : { get  : function() { return base.readyState  ; } },
-						"open"                 : { value: fix("open"                             ) },
-						"setRequestHeader"     : { value: fix("setRequestHeader"                 ) },
-						"upload"               : { get  : function() { return base.upload      ; } },
-						"send"                 : { value: fix("send"                             ) },
-						"abort"                : { value: fix("abort"                            ) },
-						"status"               : { get  : function() { return base.status      ; } },
-						"statusText"           : { get  : function() { return base.statusText  ; } },
-						"getResponseHeader"    : { value: fix("getResponseHeader"                ) },
-						"getAllResponseHeaders": { value: fix("getAllResponseHeaders"            ) },
-						"overrideMimeType"     : { value: fix("overrideMimeType"                 ) },
-						"responseText"         : { get  : function() { return base.responseText; } },
-						"responseXML"          : { get  : function() { return base.responseXML ; } },
-						"addEventListener"     : { value: fix("addEventListener"                 ) }
-					}
+					{ "internal"             : { get  : function() { return internal;          } },
+					  "responseType"         : { value: internal.responseType,
+					                             enumerable: true,
+					                             writable:   true                                },
+					  "response"             : { get  : internal.getResponse                     },
+					  "readyState"           : { get  : function() { return base.readyState  ; } },
+					  "open"                 : { value: internal.open                            },
+					  "setRequestHeader"     : { value: fix("setRequestHeader"                 ) },
+					  "upload"               : { get  : function() { return base.upload      ; } },
+					  "send"                 : { value: internal.send                            },
+					  "abort"                : { value: fix("abort"                            ) },
+					  "status"               : { get  : function() { return base.status      ; } },
+					  "statusText"           : { get  : function() { return base.statusText  ; } },
+					  "getResponseHeader"    : { value: fix("getResponseHeader"                ) },
+					  "getAllResponseHeaders": { value: fix("getAllResponseHeaders"            ) },
+					  "overrideMimeType"     : { value: fix("overrideMimeType"                 ) },
+					  "responseText"         : { get  : function() { return base.responseText; } },
+					  "responseXML"          : { get  : function() { return base.responseXML ; } },
+					  "addEventListener"     : { value: overwrite("addEventListener"           ) },
+					  "removeEventListener"  : { value: overwrite("removeEventListener"        ) }  }
 				);
-				
-			}
+			};
 			
 			wrapper.prototype = new origRequest();
 			return wrapper;
